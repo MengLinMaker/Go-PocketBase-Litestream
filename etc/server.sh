db3ed6d58cf740403164b0d318e1e6830b383aa4
Meng Lin <menglinmaker@gmail.com>
2024-01-28 16:51:09 +1100

    Stop replication on DEV database

--- etc/server.sh
+++ etc/server.sh
@@ -5,8 +5,12 @@ echo "Restoring database"
 litestream restore /db/data.db
 litestream restore /db/logs.db
 
-if [ $STAGE==prod ]; then
-  echo "Replicating production database"
+if [ "$STAGE" == "prod" ]
+then
+  echo "Replicate PROD database"
   litestream replicate
+else
+  echo "Don't replicate DEV database"
+  /server.bin serve --http="127.0.0.1:8080"
 fi
 echo "Starting server"
