dcb88444fb59c74cf94f435e0bb91aacdd9e1618
Meng Lin <menglinmaker@gmail.com>
2024-01-31 00:54:50 +1100

    Remove serving logic to bash script

--- etc/server.sh
+++ etc/server.sh
@@ -8,8 +8,8 @@ litestream restore -if-db-not-exists -if-replica-exists /db/logs.db
 if [ "$STAGE" == "PROD" ]
 then
   echo "Replicate PROD database"
-  litestream replicate
+  litestream replicate &
 else
   echo "Don't replicate DEV database"
-  /server.bin serve --http="0.0.0.0:8080"
 fi
+/server.bin serve --http="0.0.0.0:8080"
