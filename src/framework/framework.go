6236beef1b935d76716197aeba04b1509e652e11
Meng Lin <menglinmaker@gmail.com>
2024-02-01 21:29:34 +1100

    Remove activity logger

--- src/framework/framework.go
+++ src/framework/framework.go
@@ -5,7 +5,6 @@ import (
 
 	"github.com/pocketbase/dbx"
 	"github.com/pocketbase/pocketbase"
-	"github.com/pocketbase/pocketbase/apis"
 	"github.com/pocketbase/pocketbase/core"
 	"github.com/pocketbase/pocketbase/daos"
 	"github.com/pocketbase/pocketbase/plugins/migratecmd"
@@ -25,8 +24,6 @@ type ServeEventHandler func(e *core.ServeEvent)
 
 func (f Framework) AddRoutes(handler ServeEventHandler) Framework {
 	f.Pb.OnBeforeServe().Add(func(e *core.ServeEvent) error {
-		logHttpRequests := apis.ActivityLogger(f.Pb)
-		e.Router.Use(logHttpRequests)
 		handler(e)
 		return nil
 	})
@@ -42,10 +39,6 @@ func (f Framework) Start() {
 	if err := f.Pb.Start(); err != nil {
 		log.Fatal(err)
 	}
-}
-
-func (f Framework) PB() *pocketbase.PocketBase {
-	return f.Pb
 }
 
 func (f Framework) Dao() *daos.Dao {
