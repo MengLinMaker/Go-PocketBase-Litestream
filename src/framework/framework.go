f5fac4e047602dab4612586a4f13e054099bc8fe
Meng Lin <menglinmaker@gmail.com>
2024-02-01 12:05:03 +1100

    Log HTTP requests

--- src/framework/framework.go
+++ src/framework/framework.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/pocketbase/dbx"
 	"github.com/pocketbase/pocketbase"
+	"github.com/pocketbase/pocketbase/apis"
 	"github.com/pocketbase/pocketbase/core"
 	"github.com/pocketbase/pocketbase/daos"
 	"github.com/pocketbase/pocketbase/plugins/migratecmd"
@@ -24,19 +25,26 @@ type ServeEventHandler func(e *core.ServeEvent)
 
 func (f Framework) AddRoutes(handler ServeEventHandler) {
 	f.Pb.OnBeforeServe().Add(func(e *core.ServeEvent) error {
+		logHttpRequests := apis.ActivityLogger(f.Pb)
+		e.Router.Use(logHttpRequests)
 		handler(e)
 		return nil
 	})
 }
 
 func (f Framework) Start() {
-	// Enable migrate
+	// Automigrate data
 	migratecmd.MustRegister(f.Pb, f.Pb.RootCmd, migratecmd.Config{
+		Dir:         "./migrations",
 		Automigrate: true,
 	})
 	if err := f.Pb.Start(); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func (f Framework) PB() *pocketbase.PocketBase {
+	return f.Pb
 }
 
 func (f Framework) Dao() *daos.Dao {
